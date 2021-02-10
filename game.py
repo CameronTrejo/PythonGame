4b64d1e0a9787d92055f1c419d4429f6474499cb
Cameron Trejo <camerontrejo@mail.weber.edu>
2021-02-10 08:52:42 -0700

    User Selection initial capabilities

--- game.py
+++ game.py
@@ -2,3 +2,20 @@
 # Feb 10, 2021
 
 # This is a game to demo functions in python
+
+# Rock Paper Scissors
+
+# Rock beats Scissors
+# Scissors beat Paper
+# Paper beats Rock
+
+# Psuedo Code --> 
+
+# User Selection -- get info from the user
+# Computer Selection -- find way for computer to select options
+# Evaluate the results
+# Declarea winner
+
+user_selection = input("What is your selection? (Rock, Paper, Scissors): ")
+#TODO Validate user selection 
+print('You chose', user_selection)
