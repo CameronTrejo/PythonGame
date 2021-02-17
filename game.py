df4131566dfd2bbb20605bb2466b246910857def
CameronTrejo2000 <70658557+CameronTrejo@users.noreply.github.com>
2021-02-17 09:01:08 -0700

    Added while loop

--- game.py
+++ game.py
@@ -2,46 +2,60 @@ import random
 
 possible_choices = ['Rock', 'Paper', 'Scissors']
 
-# User selection -- get info from user
+# loop through the code until user wants to quit
+while True:
+    
+    # User selection -- get info from user
 
-user_selection = input("What is your selection? (Rock, Paper, Scissors): ")
-#TODO Validate user selection 
-print('You chose', user_selection)
+    user_selection = input("What is your selection? (Rock, Paper, Scissors): ") #TODO Validate user selection 
+    print('You chose', user_selection)
 
-# Computer Selection -- find way for computer to select options
-# computer_selection = random.choices(possible_choices)
+    # Computer Selection -- find way for computer to select options
+    computer_selection = random.choice(possible_choices)
+    
+    print('Computer chose', str(computer_selection))
 
-#TODO remove this temporary test
-computer_selection = "Rock"
+    # Evaluate the results -- Conditional statement
+    if user_selection == computer_selection:
+        print("It is a tie")
 
-print('Computer chose', str(computer_selection))
+    #----------ROCK----------
+    elif user_selection == "Rock":
+        #computer selects paper
+        if computer_selection == "Paper":
+            print("User loses")
+        #computer selects scissors
+        else:
+            print("User wins")
+
+    #----------PAPER----------
+    elif user_selection == "Paper":
+        if computer_selection == "Scissors":
+            print("User loses")
+        else:
+            print("User wins")
+
+    #----------SCISSORS----------
+    elif user_selection == "Scissors":
+        if computer_selection == "Rock":
+            print("User loses")
+        else:
+            print("User wins")
+
+    # Declare a winner
+
+    play_again = input("Do you want to play again (y/n): ")
+    if play_again == "n":
+        break
+        #break out of the loop
+
+# This is outside of the loop
+print("Thanks for playing!")
 
 
-# Evaluate the results -- Conditional statement
-if user_selection == computer_selection:
-    print("It is a tie")
 
-#----------ROCK----------
-elif user_selection == "Rock":
-    #computer selects paper
-    if computer_selection == "Paper":
-        print("User loses")
-    #computer selects scissors
-    else:
-        print("User wins")
 
-#----------PAPER----------
-elif user_selection == "Paper":
-    if computer_selection == "Scissors":
-        print("User loses")
-    else:
-        print("User wins")
 
-#----------SCISSORS----------
-elif user_selection == "Scissors":
-    if computer_selection == "Rock":
-        print("User loses")
-    else:
-        print("User wins")
 
-# Declare a winner
+
+
